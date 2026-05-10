94b8c4721397645ce7b5f438d235f47ac9bf4d1f
Guilherme Sales <guilhermedesaales@gmail.com>
2026-05-10 00:39:41 -0300

    feat: add TipoRota and Consorcio properties to Linha entity
    - Added TipoRota property to Linha entity with a default value of "regular".
    - Added Consorcio property to Linha entity as a nullable string.
    - Updated database schema to include new properties in Linhas table.
    - Created migration files for adding TipoRota and Consorcio columns.
    - Modified docker-compose to use environment variables for PostgreSQL configuration.

--- NoPonto/Migrations/20260510013245_AddTipoRotaETempoRealLinha.Designer.cs
+++ NoPonto/Migrations/20260510013245_AddTipoRotaETempoRealLinha.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NoPonto.Migrations
 {
     [DbContext(typeof(TransporteDbContext))]
-    partial class TransporteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260510013245_AddTipoRotaETempoRealLinha")]
+    partial class AddTipoRotaETempoRealLinha
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -144,9 +147,6 @@ namespace NoPonto.Migrations
 
                     b.Property<string>("Codigo")
                         .IsRequired()
-                        .HasColumnType("text");
-
-                    b.Property<string>("Consorcio")
                         .HasColumnType("text");
 
                     b.Property<DateTime>("CreatedAt")
